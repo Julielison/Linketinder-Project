96465cc4a24acfd34818dc838d2bcf42020e3274
Julielison Lima <julielisonlima11@gmail.com>
2025-02-27 22:02:54 -0300

    feat: testes para adição de empresa e candidato com spock

---
+++ src/test/CadastroServiceSpec.groovy
@@ -0,0 +1,32 @@
+package test
+
+
+import main.model.Candidato
+import main.model.Empresa
+import spock.lang.Specification
+import main.repository.*
+
+class CadastroServiceSpec extends Specification {
+
+    def "Deve cadastrar um novo candidato com sucesso"() {
+        given: "Um novo candidato"
+        Candidato candidato = new Candidato("Lucas Martins", "lucas@gmail.com", "12312312312", 27, "SP", "04567-890", "Desenvolvedor Backend", ["Java", "Spring Boot"])
+
+        when: "O candidato é adicionado ao repositório"
+        CandidatoRepository.addCandidato(candidato)
+
+        then: "O candidato deve estar na lista de candidatos"
+        CandidatoRepository.getCandidatos().contains(candidato)
+    }
+
+    def "Deve cadastrar uma nova empresa com sucesso"() {
+        given: "Uma nova empresa"
+        Empresa empresa = new Empresa("SmartTech", "contato@smarttech.com", "55667788000133", "Brasil", "SP", "01111-111", "Empresa de tecnologia", ["JavaScript", "React"])
+
+        when: "A empresa é adicionada ao repositório"
+        EmpresaRepository.addEmpresa(empresa)
+
+        then: "A empresa deve estar na lista de empresas"
+        EmpresaRepository.getEmpresas().contains(empresa)
+    }
+}
