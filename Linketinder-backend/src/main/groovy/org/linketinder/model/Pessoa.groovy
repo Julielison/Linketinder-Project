28dac5469a582a7b841f806de4e0866074775812
Julielison Lima <julielisonlima11@gmail.com>
2025-03-24 01:01:50 -0300

    update(model): novos atributos

--- Linketinder-backend/src/main/groovy/org/linketinder/model/Pessoa.groovy
+++ Linketinder-backend/src/main/groovy/org/linketinder/model/Pessoa.groovy
@@ -1,18 +1,38 @@
 package org.linketinder.model
 
 abstract class Pessoa implements PessoaInterface {
+    Integer id
     String nome
     String email
+    String cep
+    String descricao
+    String senhaLogin
+    String paisOndeReside
 
-    Pessoa(String nome, String email) {
+    Pessoa(Integer id,
+           String nome,
+           String email,
+           String cep,
+           String descricao,
+           String senhaLogin,
+           String paisOndeReside)
+    {
+        this.id = id
         this.nome = nome
         this.email = email
+        this.cep = cep
+        this.descricao = descricao
+        this.senhaLogin = senhaLogin
+        this.paisOndeReside = paisOndeReside
     }
 
-    String getNome() {
-        return nome
-    }
-    String getEmail() {
-        return email
+    @Override
+    String toString() {
+        return """
+        Nome: ${nome}
+        Email: ${email}
+        CEP: ${cep}
+        País Onde Reside: ${paisOndeReside}
+        Descrição: ${descricao}"""
     }
 }
---
+++ Linketinder-backend/src/main/groovy/org/linketinder/model/Vaga.groovy
@@ -0,0 +1,32 @@
+package org.linketinder.model
+
+class Vaga {
+    Integer id
+    String nome
+    String descricao
+    String local
+    Integer idEmpresa
+
+    Vaga(Integer id,
+         String nome,
+         String descricao,
+         String local,
+         Integer idEmpresa)
+    {
+        this.id = id
+        this.nome = nome
+        this.descricao = descricao
+        this.local = local
+        this.idEmpresa = idEmpresa
+    }
+
+    @Override
+    String toString() {
+        return """
+        Nome da Vaga: ${nome}
+        Descrição: ${descricao}
+        Local: ${local}
+        """
+    }
+
+}
