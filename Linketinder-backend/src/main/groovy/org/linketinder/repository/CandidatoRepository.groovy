867eb9e90e291d355a11945f1739353001087b05
Julielison Lima <julielisonlima11@gmail.com>
2025-03-24 00:43:10 -0300

    update(view): listagem de todos os dados dos candidatos

--- Linketinder-backend/src/main/groovy/org/linketinder/repository/CandidatoRepository.groovy
+++ Linketinder-backend/src/main/groovy/org/linketinder/repository/CandidatoRepository.groovy
@@ -9,21 +9,39 @@ class CandidatoRepository {
     static List<Candidato> getCandidatos() {
         candidatos.clear()
         Sql sql = DatabaseConnection.getInstance()
+        String query = """
+            SELECT 
+                c.id AS candidato_id,
+                c.nome AS candidato_nome,
+                c.sobrenome AS candidato_sobrenome,
+                c.data_nascimento AS candidato_data_nascimento,
+                c.email AS candidato_email,
+                c.cpf AS candidato_cpf,
+                c.descricao_pessoal AS candidato_descricao_pessoal,
+                c.senha_de_login AS candidato_senha_de_login,
+                e.cep AS endereco_cep,
+                p.nome AS pais_nome
+            FROM 
+                CANDIDATO c
+            JOIN 
+                ENDERECO e ON c.id_endereco = e.id
+            JOIN 
+                PAIS_DE_RESIDENCIA p ON e.pais_id = p.id"""
 
         try {
-            sql.eachRow("SELECT * FROM candidato") { row ->
-                Integer idCandidato = row.id
+            sql.eachRow(query) { row ->
+                Integer idCandidato = row.candidato_id
 
                 Candidato candidato = new Candidato(
                         idCandidato,
-                        row.nome as String,
-                        row.email as String,
-                        row.cpf as String,
-                        row.data_nascimento as Date,
-                        "fixo",
-                        row.descricao_pessoal as String,
-                        row.senha_de_login as String,
-                        "fixo",
+                        row.candidato_nome as String,
+                        row.candidato_email as String,
+                        row.candidato_cpf as String,
+                        row.candidato_data_nascimento as Date,
+                        row.endereco_cep as String,
+                        row.candidato_descricao_pessoal as String,
+                        row.candidato_senha_de_login as String,
+                        row.pais_nome as String,
                         CompetenciaRepository.getCompetencias(idCandidato)
                 )
                 candidatos.add(candidato)
